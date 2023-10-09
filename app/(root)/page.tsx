269668414b53e849da6a321dca2f27e75525bcbe
allankruger <allanfloreskruger@gmail.com>
2023-10-10 01:40:24 +0200

    chore: add Clerk authentication and its configuration

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,3 @@
-import { Button } from "@/components/ui/button";
-
-export default function Home() {
-  return (
-    <div className="p-4">
-      <Button>Click me</Button>
-    </div>
-  );
+export default function SetupPage() {
+  return <div className="p-4">This is a protected route!</div>;
 }
